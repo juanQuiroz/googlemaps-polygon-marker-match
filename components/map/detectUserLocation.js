7f03b2f7abd016de4939094d89fab546bf2f0dd6
GJounior <7944822+GJounior@users.noreply.github.com>
2022-08-29 18:45:08 -0500

    try catch

--- components/map/detectUserLocation.js
+++ components/map/detectUserLocation.js
@@ -5,27 +5,54 @@ import { updateUserMarker, updateCenterMap } from '../../store/deliveryActions'
 function DetectUserLocation(){
   const [delivery, dispatch] = useContext(DeliveryContext);
 
-  function detectLocation(){
-    navigator.geolocation.getCurrentPosition(function(location){
-      const marker = {
-        lat: location.coords.latitude,
-        lng: location.coords.longitude
-      };
-
-      dispatch(updateUserMarker(marker));
-      dispatch(updateCenterMap(marker));
-
-
-      console.log("Latitude is: ", location.coords.latitude)
-      console.log("Longitude is: ", location.coords.longitude)
-    })
+  if("geolocation" in navigator){
+    console.log("activo");
   }
+  else{
+    console.log("desactivado");
+  }
+
+  function detectLocation(){
+    if(isGPSEnable){
+      try{
+        navigator.geolocation.getCurrentPosition(function(location){
+          console.log("Latitude is: ", location.coords.latitude)
+          console.log("Longitude is: ", location.coords.longitude)
+
+          const marker = {
+            lat: location.coords.latitude,
+            lng: location.coords.longitude
+          }
+
+          dispatch(updateUserMarker(marker));
+          dispatch(updateCenterMap(marker));
+        });
+      } catch(erro){
+        alert("por favor, necesitamos que active el GPS");
+      }
+    }
+    else {
+      console.log("Verifique si su gps esta activado.");
+    }
+  }
+
 
   return (
     <>
       <button style={{ background:"skyblue"}}  onClick={detectLocation}>Usar mi ubicación actual</button>
     </>
   )
+}
+
+
+
+function isGPSEnable(){
+  if("geolocation" in navigator ){
+    console.log("enable");
+    return true;
+  }
+  console.log("disable");
+  return false
 }
 
 export default DetectUserLocation;
